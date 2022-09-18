04a6887c496e61861a0c667c848ffa5b5f4e3dcc
Sam Kimbinyi <sam@kimbinyi.com>
2022-09-18 16:05:40 +0100

    Need to fix return statements

--- ast/ast.go
+++ ast/ast.go
@@ -26,6 +26,16 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode() {}
 
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
